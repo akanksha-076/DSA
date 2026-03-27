3b07334fb0c3afe4375027ecd066cff632c3eefd
Akanksha <a31695132@gmail.com>
2026-03-27 16:08:52 +0530

    DSA

--- DSA/UNIT-01/2Darray.py
+++ DSA/UNIT-01/2Darray.py
@@ -1,27 +1,20 @@
-# Program to create and print a 2D array (matrix)
 
-# Creating a 2D array (2 rows and 3 columns)
 matrix = [
-    [1, 2, 3],   # First row
-    [4, 5, 6]    # Second row
+    [1, 2, 3],   
+    [4, 5, 6]   
 ]
 
-# Printing entire matrix
 print("Complete Matrix:")
 print(matrix)
 
 print("\nMatrix Elements Row by Row:")
 
-# Loop through rows
 for i in range(len(matrix)):
     
-    # Loop through columns
     for j in range(len(matrix[i])):
         
-        # Print each element with space
         print(matrix[i][j], end=" ")
-    
-    # Move to next line after each row
     print()
+    
 
 
